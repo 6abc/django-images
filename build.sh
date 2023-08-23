219e519746f59962590d7a687c268f548615d311
6abc <paulisevil1@gmail.com>
2023-08-23 17:56:50 +0530

    init

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('ash', 'admin@example.com', '123!@#QWE')" | python manage.py shell
